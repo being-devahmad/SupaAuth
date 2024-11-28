e21b3d7eef78aa8a3f4116241d2d500b9e26ba81
Muhammad Ahmad Owais <130239694+being-devahmad@users.noreply.github.com>
2024-11-29 00:11:31 +0500

    update dashboard-layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,29 +1,15 @@
-import { GeistSans } from "geist/font/sans";
 import "../../app/globals.css";
 import Sidebar from "@/components/Dashboard/Sidebar";
 
-// export const metadata = {
-//     title: "Supabase Next Auth",
-//     description: "Authentication Management System using Supabase and Next.js",
-// };
-
-export default function RootLayout({
+export default function DashboardLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
     return (
-        <html lang="en" className={GeistSans.className}>
-            <body>
-
-                <div className="flex  h-screen bg-gray-700 text-gray-500">
-                    <Sidebar />
-                    <div className="w-full">
-                        {children}
-                    </div>
-                </div>
-
-            </body>
-        </html>
+        <div className="flex h-screen bg-gray-700 border ">
+            <Sidebar />
+            <div className="w-full">{children}</div>
+        </div>
     );
 }
